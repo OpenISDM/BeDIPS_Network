57773b0d4e5ad84945f19a8d2c72e0d9780af9a3
Gary Hsiao <26973371+garyh0205@users.noreply.github.com>
2018-02-10 12:49:48 +0800

    add decription header in each file (Draft ver.)

--- tools/LBeacon_Zigbee_Gateway.c
+++ tools/LBeacon_Zigbee_Gateway.c
@@ -1,3 +1,48 @@
+/*
+ * Copyright (c) 2016 Academia Sinica, Institute of Information Science
+ *
+ * License:
+ *
+ *      GPL 3.0 : The content of this file is subject to the terms and
+ *      cnditions defined in file 'COPYING.txt', which is part of this 
+ *      source code package.
+ *
+ * Project Name:
+ *
+ *      BeDIPS
+ *
+ * File Description:
+ *
+ *   	This file contains the program to set up a star network by XBEE S2C
+ *      module in order to deal with NSI(Network Setup and Initialization) 
+ *      and Data transmission between Gateway and LBeacon. And This file is 
+ *		for Gateway.
+ *
+ * File Name:
+ *
+ *      LBeacon_Zigbee_Gateway.c
+ *
+ * Abstract:
+ *
+ *      BeDIPS uses LBeacons to deliver 3D coordinates and textual
+ *      descriptions of their locations to users' devices. Basically, a
+ *      LBeacon is an inexpensive, Bluetooth Smart Ready device. The 3D
+ *      coordinates and location description of every LBeacon are retrieved
+ *      from BeDIS (Building/environment Data and Information System) and
+ *      stored locally during deployment and maintenance times. Once
+ *      initialized, each LBeacon broadcasts its coordinates and location
+ *      description to Bluetooth enabled user devices within its coverage
+ *      area.
+ *
+ * Authors:
+ *      Jake Lee		, jakelee@iis.sinica.edu.tw
+ *      Johnson Su		, johnsonsu@iis.sinica.edu.tw
+ *      Shirley Huang	, shirley.huang.93@gmail.com
+ *      Han Hu			, hhu14@illinois.edu
+ *      Jeffrey Lin		, lin.jeff03@gmail.com
+ *      Howard Hsu		, haohsu0823@gmail.com
+ *      Gary Xiao		, garyh0205@hotmail.com
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
